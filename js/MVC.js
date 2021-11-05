ac9b0f3b430f0dd1a5ae399f7b3e827afcb75ba1
Anastasia Bekasova <anastasia.bekasova@mail.ru>
2021-11-05 13:09:51 +0300

    Removing unused code and file

--- js/MVC.js
+++ js/MVC.js
@@ -1,93 +1,70 @@
 class Model {
   constructor() {
     this.registeredUser = {};
-    this.savedUser = {}
+    this.savedUser = {};
   }
 
   signUp = (inputs) => {
     let userData = {};
     for (let input of inputs) {
-     userData[input.name] = input.value;
+      userData[input.name] = input.value;
     }
-    localStorage.setItem(userData.email, JSON.stringify(userData))
-    console.log( userData)
-console.log('Your registration is completed successfully!');
-alert('Your registration is completed successfully!')
-  }
-    
+    localStorage.setItem(userData.email, JSON.stringify(userData));
+    console.log(userData);
+    console.log("Your registration is completed successfully!");
+    alert("Your registration is completed successfully!");
+  };
+
   signIn = (inputs) => {
-  
     let login;
     let confirmToEnter;
     for (let input of inputs) {
       if (input.name === "email") {
-        login = input.value
-      } 
+        login = input.value;
+      }
     }
-    let userData =JSON.parse(localStorage.getItem(login));
+    let userData = JSON.parse(localStorage.getItem(login));
     let errorMessage;
     if (!userData) {
-      errorMessage = 'You are not registered yet.'
-      return errorMessage
+      errorMessage = "You are not registered yet.";
+      return errorMessage;
     }
-    confirmToEnter = Object.values(inputs).every( input =>  { return userData[input.name] == input.value} );
-     if (confirmToEnter) {
-      // window.open('doctor-view.html')
+    confirmToEnter = Object.values(inputs).every((input) => {
+      return userData[input.name] == input.value;
+    });
+    if (confirmToEnter) {
       const Doctor = new ControllerDoctor(new ModelDoctor(), new ViewDoctor());
-     } else {
-      errorMessage = 'Password is wrong. Please check the spelling'
-      return errorMessage
-     } 
-      // console.log('Password is wrong. Please check the spelling')
-    // if (userData) {
-    //   confirmToEnter = Object.values(inputs).every( input =>  { return userData[input.name] == input.value} );
-    //   confirmToEnter ? window.open('doctor-view.html') : console.log('Password is wrong. Please check the spelling')
-    // } else {
-    //   console.log('You are not registered yet.')
-    // } 
-     
-  }
+    } else {
+      errorMessage = "Password is wrong. Please check the spelling";
+      return errorMessage;
+    }
+  };
 }
 
 class View {
   constructor() {
     this.main = this.getElement(".enter-panel");
     this.routes = {
-      "sign-up" : signUp,
-      "sign-in" : signIn,
-      "restore-password" :  restorePassword
-    }
+      "sign-up": signUp,
+      "sign-in": signIn,
+      "restore-password": restorePassword,
+    };
   }
 
   getElement(selector) {
     const element = document.querySelector(selector);
     return element;
   }
+
   createElement = (tag, className) => {
     const element = document.createElement(tag);
     if (className) element.classList.add(className);
     return element;
   };
-  changePage = (page) => {
 
-    let state = { page: page };
-  window.history.replaceState(state, "", state.page);
-  this.displayPage(state.page);
-      };
-
-  displayPage = () => {
-    this.main.innerHTML = "";
-    if (!window.history.state) return;  
-    this.main.innerHTML = this.routes[window.history.state.page];
-     this.findElements();
-    this.inputSetup();
-    this.goToListener();
-    this.togglerListener();
-  };
   findElements = () => {
     this.linksNode = document.querySelectorAll(".link");
     this.inputsNode = document.querySelectorAll("input");
-    // this.submit = document.querySelector(".submit");
     this.passwordInputsNode = document.querySelectorAll(`[name = 'password']`);
     this.form = document.querySelector(".form");
     this.togglersNode = document.querySelectorAll(".toggler-display-password");
@@ -96,17 +73,7 @@ class View {
   //validating methods
 
   markInput = (input, valid) => {
-    // if (valid) {
-    //   input.classList.add('form__input_valid');
-    //   return true
-    // }
-    // if (!valid) {
-    //   input.classList.add('form__input_invalid');    
-    //   return false
-    // }
-    
-    const validationClass = valid ? 'form__input_valid' : 'form__input_invalid';
-  
+    const validationClass = valid ? "form__input_valid" : "form__input_invalid";
     input.classList.add(validationClass);
   };
 
@@ -120,52 +87,71 @@ class View {
         input.setAttribute("minlength", "2");
       }
     }
-  }
+  };
 
   createErrorMessage = (text, parentElement) => {
     const error = this.createElement("div", "error-message");
     error.innerHTML = text;
     parentElement.append(error);
-  }
+  };
 
   checkPasswordValidity = (node) => {
-    // if (document.querySelector('.error-message')) document.querySelector('.error-message').remove();
     const [passwordStep1, passwordStep2] = node;
     let isValid;
-    if(passwordStep1.value === passwordStep2.value && passwordStep1.value) {
-      this.markInput(passwordStep2, true)
+    if (passwordStep1.value === passwordStep2.value && passwordStep1.value) {
+      this.markInput(passwordStep2, true);
       isValid = true;
     } else if (passwordStep2.value) {
-      const errorMessage = 'Passwords are not equal. Please check the spelling'
+      const errorMessage = "Passwords are not equal. Please check the spelling";
       this.createErrorMessage(errorMessage, passwordStep2.parentElement);
       this.markInput(passwordStep2, false);
       isValid = false;
     }
-    return isValid
-   }
+    return isValid;
+  };
 
   checkValidityForm = () => {
-    if (document.querySelector('.error-message')) document.querySelector('.error-message').remove();
+    if (document.querySelector(".error-message"))
+      document.querySelector(".error-message").remove();
     for (let input of this.inputsNode) {
-      this.removeValidateClasses(input)
+      this.removeValidateClasses(input);
     }
-    const isFormValid = Object.values(this.inputsNode).every(input => {
+    const isFormValid = Object.values(this.inputsNode).every((input) => {
       if (input === this.passwordInputsNode[1]) {
-        return this.checkPasswordValidity(this.passwordInputsNode)
+        return this.checkPasswordValidity(this.passwordInputsNode);
       }
       const isValid = input.checkValidity();
-      this.markInput(input, isValid) 
-       return isValid
-      })
-      return isFormValid;
-      }
-      
+      this.markInput(input, isValid);
+      return isValid;
+    });
+    return isFormValid;
+  };
+
+  removeValidateClasses = (input) => {
+    input.classList.contains("form__input_valid") &&
+      input.classList.remove("form__input_valid");
+    input.classList.contains("form__input_invalid") &&
+      input.classList.remove("form__input_invalid");
+  };
+
+  resetFormListeners = (event) => {
+    for (let input of this.inputsNode) {
+      input.addEventListener(
+        "change",
+        () => {
+          this.removeValidateClasses(input);
+        },
+        true
+      );
+    }
+  };
+
   //routing methods
 
   goToListener = () => {
     for (let link of this.linksNode) {
       link.addEventListener("click", (event) => {
-        this.goTo(event)
+        this.goTo(event);
       });
     }
   };
@@ -173,67 +159,76 @@ class View {
   goTo = (event) => {
     event.preventDefault();
     let state;
-      if (event.currentTarget.getAttribute("href") === "back") {      
-    window.history.back();
-    state = window.history.state;
-  } else {
-    state = { page: event.currentTarget.getAttribute("href") };
-    window.history.pushState(state, "", state.page);
-  }
-  this.displayPage(state.page);
+    if (event.currentTarget.getAttribute("href") === "back") {
+      window.history.back();
+      state = window.history.state;
+    } else {
+      state = { page: event.currentTarget.getAttribute("href") };
+      window.history.pushState(state, "", state.page);
+    }
+    this.displayPage(state.page);
   };
 
-  onPopStateListener = () => {    
-  window.addEventListener('popstate', () => this.displayPage(window.history.state.page))
-  }
+  onPopStateListener = () => {
+    window.addEventListener("popstate", () =>
+      this.displayPage(window.history.state.page)
+    );
+  };
 
+  changePage = (page) => {
+    let state = { page: page };
+    window.history.replaceState(state, "", state.page);
+    this.displayPage(state.page);
+  };
+
+  displayPage = () => {
+    this.main.innerHTML = "";
+    if (!window.history.state) return;
+    this.main.innerHTML = this.routes[window.history.state.page];
+    this.findElements();
+    this.inputSetup();
+    this.goToListener();
+    this.togglerListener();
+  };
 
   //submit methods
+
   bindSubmitListener = (handler1, handler2) => {
-    
-this.main.addEventListener("click", (event) => { 
-  const targetClass = event.target.classList;
-  const buttonClass = event.target.parentElement.classList;
+    this.main.addEventListener("click", (event) => {
+      const targetClass = event.target.classList;
+      const buttonClass = event.target.parentElement.classList;
 
-  if (!(targetClass.contains('submit') || buttonClass.contains('submit'))) return;
-    event.preventDefault();
-    const isFormValid = this.checkValidityForm();
-    this.resetFormListeners(event);
-    let handler;
-  
-  if (targetClass.contains('submit-sign-up') || buttonClass.contains('submit-sign-up'))  {    
-    handler =  handler1;
-    }
-  if (targetClass.contains('submit-sign-in') || buttonClass.contains('submit-sign-in'))  {
-    handler =  handler2;
-  }
-  isFormValid ? handler(event) : null;
-})
-}
+      if (!(targetClass.contains("submit") || buttonClass.contains("submit")))
+        return;
+      event.preventDefault();
+      const isFormValid = this.checkValidityForm();
+      this.resetFormListeners(event);
+      let handler;
 
-removeValidateClasses = (input) => {
-    input.classList.contains('form__input_valid') && input.classList.remove('form__input_valid');
-    input.classList.contains('form__input_invalid') && input.classList.remove('form__input_invalid')
-    
-  }
+      if (
+        targetClass.contains("submit-sign-up") ||
+        buttonClass.contains("submit-sign-up")
+      ) {
+        handler = handler1;
+      }
+      if (
+        targetClass.contains("submit-sign-in") ||
+        buttonClass.contains("submit-sign-in")
+      ) {
+        handler = handler2;
+      }
+      isFormValid ? handler(event) : null;
+    });
+  };
 
-  resetFormListeners = (event) => {
 
-    for (let input of this.inputsNode) {
-      input.addEventListener('change', () => {
-        this.removeValidateClasses(input)
-      }, true
-        )
-    }
-  }
-
-//displaying password methods
+  //displaying password methods
 
   togglerListener = () => {
     for (let toggler of this.togglersNode) {
       toggler.addEventListener("click", this.toggleDisplayPassword);
     }
-  }
+  };
   toggleDisplayPassword = (event) => {
     event.preventDefault();
     let button = event.currentTarget;
@@ -242,7 +237,7 @@ removeValidateClasses = (input) => {
     let input = button
       .closest(".form__input-wrapper")
       .querySelector(".form__input");
-  
+
     if (button.classList.contains("show-password")) {
       input.type = "password";
       icon.src = "img/svg/eye-crossed-out.svg";
@@ -251,8 +246,7 @@ removeValidateClasses = (input) => {
       icon.src = "img/svg/eye.svg";
     }
   };
-
-};
+}
 
 class Controller {
   constructor(model, view) {
@@ -260,23 +254,21 @@ class Controller {
     this.view = view;
     this.view.changePage("sign-up");
     this.view.onPopStateListener();
-    this.view.bindSubmitListener(this.handleSignUp, this.handleSignIn)
+    this.view.bindSubmitListener(this.handleSignUp, this.handleSignIn);
   }
 
   handleSignUp = () => {
-this.model.signUp(this.view.inputsNode);
-this.view.changePage("sign-in");
-
-  }
+    this.model.signUp(this.view.inputsNode);
+    this.view.changePage("sign-in");
+  };
   handleSignIn = () => {
-        const errorMessage = this.model.signIn(this.view.inputsNode);
-         if (errorMessage) {
-           const passwordInput = this.view.passwordInputsNode[0]
-          this.view.createErrorMessage(errorMessage, passwordInput.parentElement);
-          this.view.markInput(passwordInput, false);
-         }
-        
-      }
+    const errorMessage = this.model.signIn(this.view.inputsNode);
+    if (errorMessage) {
+      const passwordInput = this.view.passwordInputsNode[0];
+      this.view.createErrorMessage(errorMessage, passwordInput.parentElement);
+      this.view.markInput(passwordInput, false);
+    }
+  };
 }
 
 const app = new Controller(new Model(), new View());
